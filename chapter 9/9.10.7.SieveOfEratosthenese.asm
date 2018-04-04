e4d2f19136a606095516c0c5bb568aba3f2fb27a
x86assembly <37060921+x86assembly@users.noreply.github.com>
2018-04-04 12:29:23 -0700

    Add files via upload

---
+++ chapter 9/9.10.7.SieveOfEratosthenese.asm
@@ -0,0 +1,25 @@
+; 9.10.6.FrequencyTable
+include link.inc
+include macros.inc
+
+
+
+
+.data
+disp1 byte "This program demonstrates a procedure which uses the Sieve of Erotosthenes method to display primes up to 65,000", 0ah,0dh, 0ah,0dh,0
+
+primes byte 65000d dup(?)
+.code
+	main proc
+		invoke displayString, addr disp1
+		invoke InitializeArray, addr primes, lengthof primes, 0
+		invoke sievePrimes, addr primes, lengthof primes
+		invoke displayUnmarkedElements, addr primes, lengthof primes
+		
+	call waitmsg
+	exit
+	main endp
+
+
+end main
+	
